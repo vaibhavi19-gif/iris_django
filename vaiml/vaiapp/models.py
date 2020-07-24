58a269e2d52567786429e5de6bd622373f21491d
Vaibhavi Kundle <60182895+vaibhavi19-gif@users.noreply.github.com>
2020-07-24 02:34:13 -0700

    Add files via upload

---
+++ vaiml/vaiapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+
+class PredResults(models.Model):
+    sepal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    petal_length = models.FloatField()
+    petal_width = models.FloatField(default=0.0)
+    classification = models.CharField(max_length=30)
+
+
+def __str__(self):
+    return self.classification
